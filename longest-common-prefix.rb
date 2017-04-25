91b02b1d4c9a5124a78cb9cc870ca9b229898ee7
Violet Feng <violet.feng@canonical.com>
2017-04-25 19:44:47 +0800

    Update: add longest-common-prefix.rb

---
+++ longest-common-prefix.rb
@@ -0,0 +1,29 @@
+#!/usr/bin/env ruby
+#
+# https://leetcode.com/problems/longest-common-prefix/#/description
+#
+
+def longest_common_prefix strs
+  i = 0
+  j = 0
+
+  return "" if strs.length == 0
+  length = strs[0].length
+  
+  while j < length do
+    i = 1
+    while i < strs.length do
+      unless strs[i][j] == strs[0][j]
+        common_prefix = (j == 1) ? strs[0][0] : strs[0][0, j]
+        return "" if common_prefix.nil?
+        return common_prefix
+      end
+      i += 1
+    end
+    j += 1
+  end
+
+  return strs[0][0, j]
+end
+
+puts (longest_common_prefix ["123", "123w", "1234"])
